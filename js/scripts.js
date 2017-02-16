5febb535f1a904dca863df90cf0f3ed4365c8798
Janek Brandt and Stormi Hoebelheinrich <stormi@>
2017-02-16 15:25:55 -0800

    add more style

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,13 @@
 $(function(){
   $("#heightButton").click(function() {
     var height = parseInt(prompt("Enter your height in inches"));
-    $(".ride").removeClass("highlightOn");
+    $(".ride").removeClass("highlightOn crossOut");
     if (height > 60) {
       $(".tall").addClass("highlightOn");
+      $(".short").addClass("crossOut");
     } else if (height < 60) {
       $(".short").addClass("highlightOn");
+      $(".tall").addClass("crossOut");
     } else {
       $(".short, .tall").addClass("highlightOn");
     }
